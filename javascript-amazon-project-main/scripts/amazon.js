be6c5b36178df1911a66961c8b82f7a42903bb7b
Kishik <kishikkhajuria03@gmail.com>
2026-03-07 22:25:46 +0530

    Added a "ADDED" message when added to cart

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -43,7 +43,7 @@ products.forEach((product) => {
 
           <div class="product-spacer"></div>
 
-          <div class="added-to-cart">
+          <div class="added-to-cart js-added-to-cart-${product.id}">
             <img src="images/icons/checkmark.png">
             Added
           </div>
@@ -56,6 +56,8 @@ products.forEach((product) => {
   `;
 });
   
+const addedMessageTimeouts = {};
+
 document.querySelector('.js-products-grid').innerHTML = productsHTML
 
 document.querySelectorAll('.js-add-to-cart')
@@ -94,6 +96,23 @@ document.querySelectorAll('.js-add-to-cart')
       
     document.querySelector('.js-cart-quantity')
         .innerHTML = cartQuantity;
+
+    const addedMessage = document.querySelector(
+      `.js-added-to-cart-${productId}`
+    );
+
+    addedMessage.classList.add('added-to-cart-visible');
+
+      const previousTimeoutId = addedMessageTimeouts[productId];
+      if (previousTimeoutId) {
+        clearTimeout(previousTimeoutId);
+      }
+
+      const timeoutId = setTimeout(() => {
+      addedMessage.classList.remove('added-to-cart-visible');
+    }, 2000);
+
+    addedMessageTimeouts[productId] = timeoutId;
   });
 });
 
