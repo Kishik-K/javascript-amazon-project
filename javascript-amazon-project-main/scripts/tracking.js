f34400faf1690636bb88213e2dda08bd14288934
Kishik <kishikkhajuria03@gmail.com>
2026-04-05 20:16:29 +0530

    //extra comments

--- javascript-amazon-project-main/scripts/tracking.js
+++ javascript-amazon-project-main/scripts/tracking.js
@@ -26,8 +26,12 @@ async function loadPage() {
   const deliveryTime = dayjs(productDetails.estimatedDeliveryTime);
   const percentProgress =
     ((today - orderTime) / (deliveryTime - orderTime)) * 100;
+
+  // Extra feature: display "delivered" on the tracking page
+  // if today's date is past the delivery date.
   const deliveredMessage =
     today < deliveryTime ? "Arriving on" : "Delivered on";
+
   const trackingHTML = `
     <a class="back-to-orders-link link-primary" href="orders.html">
       View all orders
